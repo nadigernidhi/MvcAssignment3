618062f6cc1f8594b6aa40c7eb7f098f04e83036
nadigernidhi <nnadiger02@gmail.com>
2022-05-03 17:11:49 +0000

    Assignment3 committed

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AspAssignment11.MvcApplication" Language="C#" %>
---
+++ Models/MyDbContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient;
+using System.Data.Entity;
+
+namespace AspAssignment11.Models
+{
+    public class MyDbContext : DbContext
+
+    {
+       public MyDbContext() : base("name=conn")
+        {
+
+        }
+       public DbSet<User> Users { get; set; }
+
+    }
+
+}
