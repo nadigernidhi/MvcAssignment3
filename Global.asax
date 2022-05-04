1d0ea349f0ddfff1f23adb10a7b4a55c83f25f78
nadigernidhi <102419893+nadigernidhi@users.noreply.github.com>
2022-05-04 07:38:23 +0000

    Add files via upload

--- Global.asax
+++ Global.asax
@@ -1,1 +1,1 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AssignmentDay2.MvcApplication" Language="C#" %>
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Assignment6.MvcApplication" Language="C#" %>
--- Global.asax.cs
+++ Global.asax.cs
@@ -1,18 +1,21 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Optimization;
 using System.Web.Routing;
 
-namespace AssignmentDay2
+namespace Assignment6
 {
     public class MvcApplication : System.Web.HttpApplication
     {
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
     }
 }
