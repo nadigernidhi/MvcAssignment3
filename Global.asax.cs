f7536d04effeedab1e0d166921e63fd3968cda01
nadigernidhi <102419893+nadigernidhi@users.noreply.github.com>
2022-05-04 07:35:28 +0000

    Add files via upload

--- Global.asax.cs
+++ Global.asax.cs
@@ -5,7 +5,7 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
 
-namespace Assignment4
+namespace AssignmentDay2
 {
     public class MvcApplication : System.Web.HttpApplication
     {
